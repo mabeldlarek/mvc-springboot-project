c0070f9c8f1d6ac882830ba24f9d599196da13a2
Maria Izabel <izabel.162@hotmail.com>
2023-08-17 15:44:20 -0300

    add racing query search

--- web/src/main/java/com/mabeldlarek/web/controller/ClubController.java
+++ web/src/main/java/com/mabeldlarek/web/controller/ClubController.java
@@ -8,10 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,6 +25,13 @@ public class ClubController {
     @GetMapping("/clubs")
     public String listClubs(Model model) {
         List<ClubDto> clubs = clubService.findAllClubs();
+        model.addAttribute("clubs", clubs);
+        return "clubs-list";
+    }
+
+    @GetMapping("/clubs/search")
+    public String searchClubs(@RequestParam(value="query") String query, Model model) {
+        List<ClubDto> clubs = clubService.searchClubs(query);
         model.addAttribute("clubs", clubs);
         return "clubs-list";
     }
